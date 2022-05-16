de6f5639c697c8751531af41d029e80f77fff813
Suraj Saroj <sarojsuraj@gmail.com>
2022-05-17 00:00:34 +0530

    Added ReactDOM.createPortals

--- src/component/UI/ErrorModel.js
+++ src/component/UI/ErrorModel.js
@@ -1,10 +1,15 @@
 import classes from "./ErrorModel.module.css";
 import Button from "./Button";
 import Card from "./Card";
+import React from "react";
+import  ReactDOM  from "react-dom"
 
-const ErrorModel = (props) => {
-  return (<div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
     <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
@@ -14,8 +19,14 @@ const ErrorModel = (props) => {
         <Button onClick={props.onConfirm}>Okay</Button>
       </footer>
     </Card>
-  </div>
   );
+};
+
+const ErrorModel = (props) => {
+  return <React.Fragment>
+  {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />,document.getElementById('overlay-root'))}
+  {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />,document.getElementById('modal-root'))}
+  </React.Fragment>
 };
 
 export default ErrorModel;
